b39f34901a5381468972f1267c45764c4f38d6f2
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2026-04-19 00:00:47 +0200

    Add support page

---
+++ app/support/page.tsx
@@ -0,0 +1,17 @@
+export default function SupportPage() {
+  return (
+    <main>
+      <section>
+        <h1>Support</h1>
+        <p>
+          If you&lsquo;re experiencing issues, contact us via support in the app
+          or at{" "}
+          <a href="mailto:support@golero.app" target="_blank">
+            support@golero.app
+          </a>
+          .
+        </p>
+      </section>
+    </main>
+  );
+}
